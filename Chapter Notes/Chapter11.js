5a55f90d1fe1effab8b06116c848790d323fe11b
Brennan Glynn <BrennanGlynn@u.boisestate.edu>
2017-03-05 16:05:59 -0700

    Mixins

--- Chapter Notes/Chapter11.js
+++ Chapter Notes/Chapter11.js
@@ -2,7 +2,61 @@
 //                          Chapter 11                          //
 //                           Functions                          //
 //////////////////////////////////////////////////////////////////
+var __slice = Array.prototype.slice;
 
+function variadic (fn) {
+    var fnLength = fn.length;
+
+    if (fnLength < 1) {
+        return fn;
+    }
+    else if (fnLength === 1) {
+        return function () {
+            return fn.call(
+                this, __slice.call(arguments, 0));
+        }
+    }
+    else {
+        return function () {
+            var numberOfArgs = arguments.length,
+                namedArgs = __slice.call(
+                    arguments, 0, fnLength - 1),
+                numberOfMissingArgs = Math.max(
+                    fnLength - numberOfArgs - 1, 0),
+                argPadding = new Array(numberOfMissingArgs),
+                variadicArgs = __slice.call(
+                    arguments, fn.length - 1);
+            return fn.apply(
+                this, namedArgs
+                    .concat(argPadding)
+                    .concat([variadicArgs]));
+
+        }
+    }
+};
+
+var extend = variadic( function (consumer, providers) {
+    var key,
+        i,
+        provider;
+
+    for (i = 0; i < providers.length; i++) {
+        provider = providers[i];
+        for (key in provider) {
+            if (provider.hasOwnProperty(key)) {
+                consumer[key] = provider[key]
+            }
+        }
+    }
+    return consumer;
+});
+
+function fluent (methodBody) {
+    return function () {
+        methodBody.apply(this, arguments);
+        return this;
+    }
+}
 //////////////////////////////////////////////////////////////////
 //                          Chapter 11                          //
 //                           New Ideas                          //
@@ -97,3 +151,53 @@ console.log(new User2('Brennan', 'Glynn'));
 //////////////////////////////////////////////////////////////////
 //                            Mixins                            //
 //////////////////////////////////////////////////////////////////
+
+function Todo (name) {
+    var self = this instanceof Todo ? this : new Todo();
+    self.name = name || 'Untitled';
+    self.done = false;
+    return self;
+}
+
+Todo.prototype.do = fluent( function () {
+    this.done = true;
+});
+
+Todo.prototype.undo = fluent( function () {
+    this.done = false;
+});
+
+// var ColorCoded = {
+//     setColorRGB: fluent( function (r, g, b) {
+//         this.colorCode = {r: r, g: g, b: b };
+//     }),
+//     getColorRGB: function () {
+//         return this.colorCode;
+//     }
+// };
+//
+// extend(Todo.prototype, ColorCoded);
+// console.log(Todo.prototype);
+
+// A simple definition that works for most purposes is to define a mixin as: A collection of
+// behaviour that can be added to a class’s existing prototype
+
+// The mixin we have above works properly, but our little recipe had two distinct steps
+// 1. Define the mixin
+// 2. Extend the class prototype
+
+// It's more elegant to define a mixin as a function than an object
+// This is called a functional mixin
+
+function becomeColorCoded (target) {
+    target.setColorRGB = fluent( function (r, g, b) {
+        this.colorCode = { r: r, g: g, b: b };
+    });
+    target.getColorRGB = function () {
+        return this.colorCode;
+    };
+    return target;
+}
+
+becomeColorCoded(Todo.prototype);
+console.log(Todo.prototype);
